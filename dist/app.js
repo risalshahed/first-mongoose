f7096a389ee582af7095eb7bb81fb8ee6db07d44
Risal <bro@BROs-Mac-mini.local>
2025-05-23 21:34:14 +0600

    setup finished

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,19 @@
 "use strict";
-const express = require('express');
-const app = express();
-const port = 3000;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+// Prettier Code to indent & also remove unnecessary empty lines & spaces
+// npx prettier --write src/app.ts
+const app = (0, express_1.default)();
+// parsers
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+// Directory of the Project Root
 console.log(process.cwd());
+exports.default = app;
--- dist/server.js
+++ dist/server.js
@@ -8,12 +8,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const mongoose = require('mongoose');
-function main() {
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("./app"));
+const config_1 = __importDefault(require("./app/config"));
+const mongoose_1 = __importDefault(require("mongoose"));
+(function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield mongoose.connect(process.env.DATABSE_URL);
+        try {
+            yield mongoose_1.default.connect(config_1.default.database_url);
+            app_1.default.listen(config_1.default.port, () => {
+                console.log(`Example app listening on port ${config_1.default.port}`);
+            });
+        }
+        catch (err) {
+            console.error(err);
+        }
     });
-}
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+})();
